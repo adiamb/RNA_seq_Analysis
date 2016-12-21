5f75e3b8b67d1b33c1758b048e24e629199da364
adiamb <ambati@stanford.edu>
2016-12-21 10:56:47 -0800

    commit at 1056am

--- RNA_Seq_Analysis_Dec21.R
+++ RNA_Seq_Analysis_Dec21.R
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 ##RNA Seq differential expression analysis
 require(readr)
 require(plyr)
 require(data.table)
 require(dplyr)
 rna_seq = read_delim('~/Documents/RNA_Seq/RNA_seq_Analysis_Dec21/tpm.diff.cell.diff.stim.txt', delim = "\t")
+=======
+##RNA Seq differential expression analysis
+>>>>>>> 63055afdfdadd9d6e28467ffb0eb39417ee088f3
