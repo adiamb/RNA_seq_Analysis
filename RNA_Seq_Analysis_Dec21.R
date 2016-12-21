4d5a744921c0ab9c10dc5fffcf7c0fe7dc928741
adiamb <ambati@stanford.edu>
2016-12-21 11:14:58 -0800

    commit3

--- RNA_Seq_Analysis_Dec21.R
+++ RNA_Seq_Analysis_Dec21.R
@@ -5,6 +5,9 @@ require(plyr)
 require(data.table)
 require(dplyr)
 rna_seq = read_delim('~/Documents/RNA_Seq/RNA_seq_Analysis_Dec21/tpm.diff.cell.diff.stim.txt', delim = "\t")
+names(rna_seq)
+##make delta expression by subtracting the expression from pandemrix stimulation the control expression 
+
 =======
 ##RNA Seq differential expression analysis
 >>>>>>> 63055afdfdadd9d6e28467ffb0eb39417ee088f3
